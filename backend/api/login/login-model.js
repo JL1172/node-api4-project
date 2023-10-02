3cfd7f3bcf771acf344244d02c40cc09c5114900
Jacob Lang <jacoblang72@comcast.net>
2023-10-02 17:56:59 -0400

    wipwiwp

--- backend/api/login/login-model.js
+++ backend/api/login/login-model.js
@@ -4,7 +4,6 @@ module.exports = {
     async find(credentials) {
         const {password, username} = credentials;
         let decider = data.findIndex(item => item.password === password);
-        console.log(decider)
         if (decider !== -1) {
             let secondDecider = data.at(decider);
             return secondDecider.username === username ? true : false; 
